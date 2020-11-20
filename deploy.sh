264e5ea7087732b0fc2d6148e8257ff13369d991
Graeme Boy <graemelearn@protonmail.com>
2020-11-19 21:06:39 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # smartweave create [SRC LOCATION] [INITIAL STATE FILE] --key-file [YOUR KEYFILE]
-smartweave create "./dist/index.js" "./contracts/nft/fixtures/contract-state.json" --key-file "./wallet/keyfile.json"
+smartweave create "./dist/index.js" "./fixtures/contract-state.json" --key-file "./wallet/keyfile.json"
